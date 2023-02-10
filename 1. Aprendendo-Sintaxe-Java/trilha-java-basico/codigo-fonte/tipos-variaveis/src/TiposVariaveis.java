1e931bf7915fbe769aa0b1d9812b678a8ffe3ddb
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2023-02-10 17:33:53 -0300

    first commit

---
+++ 1. Aprendendo-Sintaxe-Java/trilha-java-basico/codigo-fonte/tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,8 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+      int a = 3;
+      int b = 4;
+
+      System.out.println("Hello " + "You");
+    }
+}
