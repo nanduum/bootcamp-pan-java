02e94233d5f0490bd1752c5f4920318263156a07
Fernando Andrade <116602686+nanduum@users.noreply.github.com>
2023-02-13 05:57:04 -0300

    att

---
+++ 1. Java/2. Lógica Condicional e Controle de Fluxo em Java/operadoresRelacionais.java
@@ -0,0 +1,42 @@
+/*
+Exercicio de Exemplo
+*/
+
+public class operadoresRelacionais {
+
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8996L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == 12 " + (i1 == i2));
+        System.out.println("i1 != 12 " + (i1 != i2));
+        System.out.println("i1 > 12 " + (i1 > i2));
+        System.out.println("i1 <= 12 " + (i1 <= i2));
+
+        System.out.println("f1 == f2 " + (f1 == f2));
+        System.out.println("f1 != f2 " + (f1 != f2));
+        System.out.println("f1 >= f2 " + (f1 > f2));
+        System.out.println("f1 < f2 " + (f1 <= f2));
+
+        System.out.println("c1 == c2 " + (c1 == c2));
+        System.out.println("c1 != c2 " + (c1 != c2));
+        System.out.println("c1 > c2 " + (c1 > c2));
+        System.out.println("c1 <= c2 " + (c1 <= c2));
+
+    }
+}
